8e157d15e3552f75ddbb966518542ccc1fa00e49
soccer00135 <ryan.john.christensen12@gmail.com>
2020-05-28 22:12:42 -0400

    Finally solved the question. It's brute force, but it works.

--- Euler/Problem_60.py
+++ Euler/Problem_60.py
@@ -30,13 +30,14 @@ def primesfrom2to(n):
             sieve[k*(k-2*(i&1)+4)//3::2*k] = False
     return np.r_[2, 3, ((3*np.nonzero(sieve)[0][1:]+1)|1)]
 
-total_primes = primesfrom2to(100)
+total_primes = primesfrom2to(30000)
 total_primes_string = list(np.char.mod('%d', total_primes))
 
-primes_check = primesfrom2to(10000000)
+primes_check = primesfrom2to(100000000)
 primes_check_string = set(np.char.mod('%d', primes_check))
 
 test_list = [3, 7, 109, 673]
+answer = [13, 5197, 5701, 6733, 8389]
 dict = {}
 for pass_one in total_primes_string:
     values = []
@@ -51,5 +52,56 @@ for pass_one in total_primes_string:
 
 second_dict = {}
 for key, value in dict.items():
-    print('key', key)
-    print('value', value)
+    for pass_one in value:
+        values = []
+        for pass_two in value:
+            after_value = pass_one + pass_two
+            before_value = pass_two + pass_one
+            if before_value in primes_check_string and after_value in primes_check_string:
+                values.append(pass_two)
+        if len(values) > 0:
+            second_dict[(key, pass_one)] = values
+        else:
+            break
+# pprint(second_dict)
+
+third_dict = {}
+for key, value in second_dict.items():
+    # print(key)
+    for pass_one in value:
+        values = []
+        for pass_two in value:
+            after_value = pass_one + pass_two
+            before_value = pass_two + pass_one
+            if before_value in primes_check_string and after_value in primes_check_string:
+                values.append(pass_two)
+        if len(values) > 0:
+            third_dict[tuple(list(key) + [pass_one])] = values
+        else:
+            break
+
+fourth_dict = {}
+for key, value in third_dict.items():
+    # print(key)
+    for pass_one in value:
+        values = []
+        for pass_two in value:
+            after_value = pass_one + pass_two
+            before_value = pass_two + pass_one
+            if before_value in primes_check_string and after_value in primes_check_string:
+                values.append(pass_two)
+        if len(values) > 0:
+            fourth_dict[tuple(list(key) + [pass_one])] = values
+        else:
+            break
+# pprint(fourth_dict)
+test_numpy = np.empty(shape=(len(fourth_dict), 5))
+counter = 0
+for key, value in fourth_dict.items():
+    final_list = sorted(list(key) + value)
+    test_numpy[counter] = np.array(final_list)
+    counter += 1
+final = sum(list(np.unique(test_numpy)))
+print("The answer is: {}".format(final))
+elapsed_time = time() - start_time
+print('The answer was found in {0:.4f} s.'.format(elapsed_time))
