aa585a4e0b644cd59420ee1e51bcec07f1ccc534
ChristensenCode <ryan.john.christensen12@gmail.com>
2024-04-22 19:17:08 -0700

    removed some unneeded stuff and started adding tests

---
+++ rusty_euler/tests/test_01.rs
@@ -0,0 +1,10 @@
+#[cfg(test)]
+mod tests {
+    // Note this useful idiom: importing names from outer (for mod tests) scope.
+    use super::*;
+
+    #[test]
+    fn test_problem_0001() {
+        assert_eq!(problem_0001(), 10);
+    }
+}
